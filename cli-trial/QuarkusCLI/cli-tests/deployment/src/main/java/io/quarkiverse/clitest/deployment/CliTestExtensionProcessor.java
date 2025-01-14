566cd3fb15585f98804cc369d465f52d928e8632
Koen Aers <koen.aers@gmail.com>
2025-01-14 10:49:30 +0100

    Rename class 'CliTestProcessor' to 'CliTestExtensionProcessor'
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

---
+++ cli-trial/QuarkusCLI/cli-tests/deployment/src/main/java/io/quarkiverse/clitest/deployment/CliTestExtensionProcessor.java
@@ -0,0 +1,25 @@
+package io.quarkiverse.clitest.deployment;
+
+import io.quarkiverse.clitest.runtime.CliTestsExtensionServlet;
+import io.quarkus.deployment.annotations.BuildStep;
+import io.quarkus.deployment.builditem.FeatureBuildItem;
+import io.quarkus.undertow.deployment.ServletBuildItem;
+
+class CliTestExtensionProcessor {
+
+    private static final String FEATURE = "cli-test-extension";
+
+    @BuildStep
+    FeatureBuildItem feature() {
+        return new FeatureBuildItem(FEATURE);
+    }
+
+    @BuildStep
+    ServletBuildItem createServlet() {
+        return ServletBuildItem
+                .builder("cli-test-extension", CliTestsExtensionServlet.class.getName())
+                .addMapping("/doit")
+                .build();
+    }
+
+}
